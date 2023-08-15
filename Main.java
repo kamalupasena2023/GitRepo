7a3d75ff78c4ed5509eaa4fe0ac46d0757cad499
Kamal Upasena <kamal@bhasha.lk>
2023-08-15 20:38:57 +0530

    Added new line to main class

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ class Main{
 	public static void main(String args[]){
 		System.out.println("Hello World..............");
 		System.out.println("This Line was added from the branch");
+		System.out.println(" I am doing this change from a IDE");
 	}
 }
